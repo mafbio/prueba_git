5ef4d376d2f61c4ab8e3091d19130cb29bc99228
Maria Argudo <maria.argudo@imdea.org>
2023-11-06 18:08:58 +0100

    Añado que pertenece al capítulo 5 del libro de R. Se muestran distintos tipos de variables

--- diamond.R
+++ diamond.R
@@ -1,4 +1,16 @@
+#Libro R capítulo 5, Exploratory Data Analysis
+
 library(tidyverse)
+
+#Visualizando distribuciones
+  # Variable categórica
 ggplot(data=diamonds)+
   geom_bar(mapping=aes(x=cut))
 #Hi, people
+diamonds %>%
+  count(cut)
+  #Variable continua
+ggplot(data=diamonds)+
+  geom_histogram(mapping = aes(x=carat), binwidth=0.5)
+diamonds %>%
+  count(cut_width(carat,0.5))
